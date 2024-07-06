b8675208efd7da782483716912a7470c24477101
Soumik Majumder <soumikcode02@gmail.com>
2024-07-06 19:47:10 +0530

    image search engine coded

---
+++ index.js
@@ -0,0 +1,56 @@
+const searchForm = document.getElementById("searchForm");
+const searchBox = document.getElementById("searchBox");
+const searchButton = document.getElementById("searchButton");
+const searchResult = document.getElementById("searchResult");
+const showMore = document.getElementById("showMore");
+
+const imageClass = ["w-[100%]", "h-[100px]", "object-cover", "rounded-[4px]", "md:h-[230px]"]
+
+const images = document.querySelectorAll("img");
+
+let keyword = "";
+let page = 1;
+const accessKey = "JHGWH5uO4VdDarWqP81TCLO6Y3HoSgLINQfUFGFygCc";
+
+async function searchNow()
+{
+    keyword = searchBox.value;
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const results = data.results;
+
+    results.forEach((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.classList.add(...imageClass);
+
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        
+        searchResult.appendChild(imageLink);
+    })
+    showMore.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchResult.innerHTML = "";
+    showMore.style.display = "none";
+    searchNow();
+})
+
+showMore.addEventListener("click", () => {
+    page++;
+    searchNow();
+})
